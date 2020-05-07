2c7a1ad41987c81d9ee471e4d18754765b805455
wjc404 <52632443+wjc404@users.noreply.github.com>
2020-05-07 11:15:28 +0800

    Update cuda_wrapper.h

--- cuda_wrapper.h
+++ cuda_wrapper.h
@@ -4,13 +4,20 @@
   if(cuda_ret != cudaSuccess){\
     printf("CUDA Error at line %d in file %s\n", __LINE__, __FILE__);\
     printf("  Error message: %s\n", cudaGetErrorString(cuda_ret));\
+    printf("  In the function call %s\n", #call);\
     exit(1);\
   }\
 }while(0)
-#define CUDA_KERNEL_CALLER(kernel_call) do{\
+#define CUDA_KERNEL_CALLER(...) do{\
   if(cudaPeekAtLastError() != cudaSuccess){\
-    printf("A CUDA error occurred above line %d in file %s\n", __LINE__, __FILE__); exit(1);\
+    printf("A CUDA error occurred prior to the kernel call %s at line %d\n", #__VA_ARGS__,  __LINE__); exit(1);\
   }\
-  kernel_call;\
-  CUDA_CALLER(cudaPeekAtLastError());\
+  __VA_ARGS__;\
+  cudaError_t cuda_ret = cudaPeekAtLastError();\
+  if(cuda_ret != cudaSuccess){\
+    printf("CUDA Error at line %d in file %s\n", __LINE__, __FILE__);\
+    printf("  Error message: %s\n", cudaGetErrorString(cuda_ret));\
+    printf("  In the kernel call %s\n", #__VA_ARGS__);\
+    exit(1);\
+  }\
 }while(0)
